30f098efbd25b7cb2295f815c7a19f97b01bef5a
Imene KOLLI <53266529+imene-swaan@users.noreply.github.com>
2023-02-14 19:44:48 +0100

    Update main.py

--- week6/main.py
+++ week6/main.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sklearn import tree
 from joblib import dump
+from sklearn.linear_model import LinearRegression
 
 
 from data import dims, counts, hyperparameters
@@ -11,6 +12,10 @@ inp=np.stack([dims,counts],axis=1)
 #train classifiers to find the best hyperparameters for a given input
 
 for k,v in hyperparameters.items():
-    model = tree.DecisionTreeClassifier(max_depth=2)
-    model = model.fit(inp, v)
-    dump(model, 'trees/' + k + '_model.joblib')
+    if k != 'lr':
+        model = tree.DecisionTreeClassifier(max_depth=2)
+        model.fit(inp, v)
+        dump(model, 'trees/' + k + '_model.joblib')
+    else:
+        model = LinearRegression().fit(inp, v)
+        dump(model, 'trees/' + k + '_model.joblib')
